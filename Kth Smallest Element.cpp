bcb398e466ceccc034b9cd8e437c6a5fd7124261
Deepanshu Sharma <69213139+deepu024@users.noreply.github.com>
2022-06-14 10:53:45 +0530

    code

---
+++ Kth Smallest Element.cpp
@@ -0,0 +1,21 @@
+class Solution{
+    public:
+    // arr : given array
+    // l : starting index of the array i.e 0
+    // r : ending index of the array i.e size-1
+    // k : find kth smallest element and return using this function
+    int kthSmallest(int arr[], int l, int r, int k) {
+        priority_queue<int> ans;
+        
+        for(int i = 0; i<k; i++){
+            ans.push(arr[i]);
+        }
+        for(int i = k; i<=r; i++){
+            if(arr[i] < ans.top()){
+                ans.pop();
+                ans.push(arr[i]);
+            }
+        }
+        return ans.top();
+    }
+};
